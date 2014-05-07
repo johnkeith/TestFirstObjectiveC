fbcda9e1cab1b86bb0ad322f1a3bef27084c2d92
John Keith <johnkeith@outlook.com>
2014-05-07 06:52:45 -0400

    Initial Commit

---
+++ TestFirstObjectiveC/TFOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TFOAppDelegate.h
+//  TestFirstObjectiveC
+//
+//  Created by John Keith on 5/7/14.
+//  Copyright (c) 2014 John Keith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TFOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestFirstObjectiveC/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestFirstObjectiveC
+//
+//  Created by John Keith on 5/7/14.
+//  Copyright (c) 2014 John Keith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TFOAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TFOAppDelegate class]));
+    }
+}
